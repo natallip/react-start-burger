3e92b9970e4faa658ea8278e2c260579775c578f
NPetkevich <NPetkevich@croc.ru>
2021-11-12 16:25:46 +0300

    add component Burger

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,36 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import MenuAdmin from './MenuAdmin';
+import Burger from './Burger';
+import sampleBurgers from '../sample-burgers';
 
 class App extends React.Component {
+  state = {
+    burgers: {},
+    order: {},
+  };
+
+  addBurger = burger => {
+    const burgers = {...this.state.burgers};
+    burgers[`burger${Date.now()}`] = burger;
+    this.setState({ burgers });
+  };
+
+  loadSampleBurgers = () => {
+    this.setState({ burgers: sampleBurgers });
+  }
+
   render() {
     return (
       <div className='burger-paradise'>
         <div className='menu'>
           <Header title='Very Hot Burger'/>
+          <ul className='burgers'>
+            {Object.keys(this.state.burgers).map((key) => <Burger key={key} index={key} details={this.state.burgers[key]}/>)}
+          </ul>
         </div>
         <Order />
-        <MenuAdmin />
+        <MenuAdmin addBurger={this.addBurger} loadSampleBurgers={this.loadSampleBurgers}/>
       </div>
     )
   }

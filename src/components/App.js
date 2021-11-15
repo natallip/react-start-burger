7c7730d19d4aded1853dc7c76514a8024be6f2a5
NPetkevich <NPetkevich@croc.ru>
2021-11-15 16:18:34 +0300

    work with components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import MenuAdmin from './MenuAdmin';
-import Burger from './Burger';
-import sampleBurgers from '../sample-burgers';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import MenuAdmin from "./MenuAdmin";
+import Burger from "./Burger";
+import sampleBurgers from "../sample-burgers";
 
 class App extends React.Component {
   state = {
@@ -11,29 +11,47 @@ class App extends React.Component {
     order: {},
   };
 
-  addBurger = burger => {
-    const burgers = {...this.state.burgers};
+  addBurger = (burger) => {
+    const burgers = { ...this.state.burgers };
     burgers[`burger${Date.now()}`] = burger;
     this.setState({ burgers });
   };
 
   loadSampleBurgers = () => {
     this.setState({ burgers: sampleBurgers });
-  }
+  };
+
+  addToOrder = (key) => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
 
   render() {
     return (
-      <div className='burger-paradise'>
-        <div className='menu'>
-          <Header title='Very Hot Burger'/>
-          <ul className='burgers'>
-            {Object.keys(this.state.burgers).map((key) => <Burger key={key} index={key} details={this.state.burgers[key]}/>)}
+      <div className="burger-paradise">
+        <div className="menu">
+          <Header title="Very Hot Burger" />
+          <ul className="burgers">
+            {Object.keys(this.state.burgers).map((key) => {
+              return (
+                <Burger
+                  key={key}
+                  index={key}
+                  addToOrder={this.addToOrder}
+                  details={this.state.burgers[key]}
+                />
+              );
+            })}
           </ul>
         </div>
-        <Order />
-        <MenuAdmin addBurger={this.addBurger} loadSampleBurgers={this.loadSampleBurgers}/>
+        <Order burgers={this.state.burgers} order={this.state.order} />
+        <MenuAdmin
+          addBurger={this.addBurger}
+          loadSampleBurgers={this.loadSampleBurgers}
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React from "react";
 
 class Burger extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  };
+
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
 
     return (
-      <li className='menu-burger'>
-        <div className='image-container'>
+      <li className="menu-burger">
+        <div className="image-container">
           <img src={image} alt={name} />
         </div>
-        <div className='burger-details'>
-          <h3 className='burger-name'>
+        <div className="burger-details">
+          <h3 className="burger-name">
             {name}
-            <span className='price'>{price} ₽</span>
+            <span className="price">{price} ₽</span>
           </h3>
           <p>{desc}</p>
-          <button className='buttonOrder'>Заказать</button>
+          <button
+            className="buttonOrder"
+            disable={!isAvailable}
+            onClick={() => {
+              this.props.addToOrder(this.props.index);
+            }}
+          >
+            {isAvailable ? "Заказать" : "Временно нет"}
+          </button>
         </div>
       </li>
-    )
+    );
   }
 }
 
